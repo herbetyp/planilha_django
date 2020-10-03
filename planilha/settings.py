9c57d3091dc4faaea2d1bee40d53ed2c45e19a24
Pbezerra-dev <herbetyp@gmail.com>
2020-10-03 17:55:58 -0300

    fix CI

--- planilha/settings.py
+++ planilha/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRETY_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
