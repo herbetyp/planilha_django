6de5ac1013c1c52ef3cb269036cb7cc9ee810b56
Pbezerra-dev <herbetyp@gmail.com>
2020-10-03 20:56:08 -0300

    add navbar and start dev template home

--- planilha/core/tests/test_home.py
+++ planilha/core/tests/test_home.py
@@ -15,3 +15,19 @@ def test_status_code(resp):
 
 def test_title(resp):
     assert_contains(resp, '<title>Home</title>')
+
+
+def test_btn_add_spent(resp):
+    assert_contains(
+        resp, '<a class="btn btn-primary btn-sm float-right" href="#">Adicionar</a>'
+    )
+
+
+def test_btn_update_spent(resp):
+    assert_contains(resp, '<a class="btn btn-warning btn-sm" href="#">Alterar</a>')
+
+
+def test_btn_remove_spent(resp):
+    assert_contains(
+        resp, '<a class="btn btn-danger btn-sm ml-1 text-white" href="#">Excluir</a>'
+    )
