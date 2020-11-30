b095fca2c38231ba875de373576cc5d6d2fb9ebd
Pbezerra-dev <herbetyp@gmail.com>
2020-11-29 22:54:28 -0300

    fix month filter

--- planilha/core/urls.py
+++ planilha/core/urls.py
@@ -7,8 +7,12 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('deletar/<int:pk>/<str:month>', views.delete_spent_view, name='delete'),
-    path('alterar/<int:pk>/<str:month>', views.update_spent_view, name='update'),
-    path('adicionar/<str:month>', views.create_spent_view, name='create'),
+    path(
+        'alterar/<int:pk>/<str:month>/<int:year>',
+        views.update_spent_view,
+        name='update',
+    ),
+    path('adicionar/<str:month>/<int:year>', views.create_spent_view, name='create'),
     path('renda/<str:month>/<int:year>', views.income_view, name='income'),
     path('mes/<str:month>/', views.month_view, name='month'),
 ]
